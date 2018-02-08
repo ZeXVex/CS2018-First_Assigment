09224a6f2ed7ae8e18830d54272259e5b516d9db
ZeXVex <32293921+ZeXVex@users.noreply.github.com>
2018-02-08 11:42:13 +0100

    Add repostori

--- src/GUI/CS2018First_Assigment.java
+++ src/GUI/CS2018First_Assigment.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cs2018.first_assigment;
+package GUI;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
